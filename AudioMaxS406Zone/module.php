5c0978a3cad828fd083fdc59fab1a001093c35a2
Joerg Oberndorf <joerg.oberndorf@obernsoft.de>
2019-06-18 21:25:54 +0200

    Adding S406

---
+++ AudioMaxS406Zone/module.php
@@ -0,0 +1,241 @@
+<?
+    class AudioMaxServerS406Zone extends IPSModule {
+
+
+ 	public function Create(){
+	        //Never delete this line!
+	        parent::Create();
+
+		$this->RegisterPropertyInteger("Zone",0);
+
+		$this->CreateVariableProfile("S406.AudioMaxVolume",1,"%",0,40,1,0,"Intensity");
+		$this->CreateVariableProfile("S406.AudioMaxGain",1,"%",0,15,1,0,"Intensity");
+		$this->CreateVariableProfile("S406.AudioMaxTone",1,"%",0,15,1,0,"Intensity");
+		$this->CreateVariableProfile("S406.AudioMaxBalance",1,"%",0,15,1,0,"Intensity");
+		$this->CreateVariableProfile("S406.AudioMaxInput",1,"",0,5,1,0,"");
+		$this->CreateVariableProfile("S406.AudioMaxMute",0,"",0,1,1,0,"Power");
+
+		$this->CreateVariableAssociation("S406.AudioMaxInput", 0, "Input 1", "Light" , 0x00FF00);
+		$this->CreateVariableAssociation("S406.AudioMaxInput", 1, "Input 2", "Light" , 0x00FF00);
+		$this->CreateVariableAssociation("S406.AudioMaxInput", 2, "Input 3", "Light" , 0x00FF00);
+		$this->CreateVariableAssociation("S406.AudioMaxInput", 3, "Input 4", "Light" , 0x00FF00);
+		$this->CreateVariableAssociation("S406.AudioMaxInput", 4, "Input 5", "Light" , 0x00FF00);
+		$this->CreateVariableAssociation("S406.AudioMaxInput", 5, "Input 6", "Light" , 0x00FF00);
+
+		$this->RegisterVariableBoolean("Power", "Room Power", "~Switch",1);
+	        $this->EnableAction("Power");
+
+		$this->RegisterVariableInteger("Volume","Volume","S406.AudioMaxVolume",2);
+		$this->EnableAction("Volume");
+
+                $this->RegisterVariableBoolean("Mute", "Mute", "~Switch",3);
+                $this->EnableAction("Mute");
+
+		$this->RegisterVariableInteger("Gain","Gain","S406.AudioMaxGain",5);
+		$this->EnableAction("Gain");
+
+		$this->RegisterVariableInteger("Balance","Balance","S406.AudioMaxBalance",6);
+		$this->EnableAction("Balance");
+
+		$this->RegisterVariableInteger("Bass","Bass","S406.AudioMaxTone",7);
+		$this->EnableAction("Bass");
+
+		$this->RegisterVariableInteger("Middle","Middle","S406.AudioMaxTone",8);
+		$this->EnableAction("Middle");
+
+		$this->RegisterVariableInteger("Treble","Treble","S406.AudioMaxTone",9);
+		$this->EnableAction("Treble");
+
+		$this->RegisterVariableInteger("Input","Input","S406.AudioMaxInput",4);
+		$this->EnableAction("Input");
+
+	}
+
+	public function ApplyChanges()
+	{
+		//Never delete this line!
+		parent::ApplyChanges();
+
+		//Connect to available splitter or create a new one
+		$this->ConnectParent("{DBA37017-532B-8269-B46F-6AF063589869}");
+
+	}
+
+        public function RequestAction($Ident, $Value) {
+
+		$Zone = $this->ReadPropertyInteger("Zone");
+
+                switch($Ident){
+                        case "Power":
+				$this->SetZonePower($Zone,$Value);
+                                break;
+                        case "Volume";
+                                $this->SetZoneVolume($Zone,$Value);
+                                break;
+			case "Mute":
+				$this->SetZoneMute($Zone,$Value);
+				break;
+			case "Input";
+				$this->SetZoneInput($Zone,$Value);
+				break;
+                        case "Gain";
+                                $this->SetZoneGain($Zone,$Value);
+                                break;
+                        case "Balance";
+                                $this->SetZoneBalance($Zone,$Value);
+                                break;
+                        case "Bass";
+                                $this->SetZoneBass($Zone,$Value);
+                                break;
+                        case "Middle";
+                                $this->SetZoneMiddle($Zone,$Value);
+                                break;
+                        case "Treble";
+                                $this->SetZoneTreble($Zone,$Value);
+                                break;
+			default:
+				break;
+                }
+		SetValue($this->GetIDForIdent($Ident), $Value);
+        }
+
+
+	public function SetZonePower($Zone,$State) {
+
+        	switch($State) {
+                	case TRUE:
+        	        	$this->Send("SET;SVR;ROO;".$Zone.";1");
+                          	break;
+
+			case FALSE:
+                        	$this->Send("SET;SVR;ROO;".$Zone.";0");
+	                	break;
+                 }
+	}
+
+	public function SetZoneVolume($Zone,$Value) {
+
+		$this->Send("SET;SVR;AUD;".$Zone.";VOL;".round(40-$Value));
+	}
+
+
+	public function SetZoneMute($Zone,$State) {
+
+			switch($State) {
+					case TRUE:
+							$this->Send("SET;SVR;AUD;".$Zone.";MUT;1");
+							break;
+
+					case FALSE:
+							$this->Send("SET;SVR;AUD;".$Zone.";MUT;0");
+							break;
+			 }
+	}
+
+	public function SetZoneInput($Zone,$Value) {
+
+		$this->Send("SET;SVR;AUD;".$Zone.";INP;".$Value);
+	}
+
+	public function SetZoneGain($Zone,$Value) {
+
+		$this->Send("SET;SVR;AUD;".$Zone.";GAI;".$Value);
+	}
+
+	public function SetZoneBalance($Zone,$Value) {
+
+		$this->Send("SET;SVR;AUD;".$Zone.";BAL;".$Value);
+	}
+
+	public function SetZoneBass($Zone,$Value) {
+
+		$this->Send("SET;SVR;AUD;".$Zone.";BAS;".$Value);
+	}
+
+	public function SetZoneMiddle($Zone,$Value) {
+
+		$this->Send("SET;SVR;AUD;".$Zone.";MID;".$Value);
+	}
+
+	public function SetZoneTreble($Zone,$Value) {
+
+		$this->Send("SET;SVR;AUD;".$Zone.";TRE;".$Value);
+	}
+
+
+
+
+	public function Send($Text)
+	{
+		$this->SendDataToParent(json_encode(Array("DataID" => "{2261C602-62C5-43DC-467F-7699B75E182E}", "Buffer" => $Text.chr(13))));
+	}
+
+	public function ReceiveData($JSONString)
+	{
+		$data = json_decode($JSONString);
+
+		$Zone = intval($data->Zone, 10);
+		$Type = $data->AudioType;
+		$Value = intval($data->AudioValue,10);
+
+		if($this->ReadPropertyInteger("Zone")==$Zone) {
+
+			switch($Type){
+				case "VOL":
+					SetValue($this->GetIDForIdent("Volume"), $Value);
+					break;
+
+				case "INP":
+					SetValue($this->GetIDForIdent("Input"), $Value);
+					break;
+
+				case "MUT":
+					SetValue($this->GetIDForIdent("Mute"), $Value);
+					break;
+
+				case "GAI":
+					SetValue($this->GetIDForIdent("Gain"), $Value);
+					break;
+
+				case "BAS":
+					SetValue($this->GetIDForIdent("Bass"), $Value);
+					break;
+
+				case "MID":
+					SetValue($this->GetIDForIdent("Middle"), $Value);
+					break;
+
+				case "TRE":
+					SetValue($this->GetIDForIdent("Treble"), $Value);
+					break;
+
+				case "BAL":
+					SetValue($this->GetIDForIdent("Balance"), $Value);
+					break;
+
+				default:
+					IPS_LogMessage("AMS404Zone","Unknown SET Audiotype: ".$Type);
+					break;
+			}
+		}
+
+	}
+
+	private function CreateVariableProfile($ProfileName, $ProfileType, $Suffix, $MinValue, $MaxValue, $StepSize, $Digits, $Icon) {
+		    if (!IPS_VariableProfileExists($ProfileName)) {
+			       IPS_CreateVariableProfile($ProfileName, $ProfileType);
+			       IPS_SetVariableProfileText($ProfileName, "", $Suffix);
+			       IPS_SetVariableProfileValues($ProfileName, $MinValue, $MaxValue, $StepSize);
+			       IPS_SetVariableProfileDigits($ProfileName, $Digits);
+			       IPS_SetVariableProfileIcon($ProfileName, $Icon);
+		    }
+	}
+
+
+	private function CreateVariableAssociation($ProfileName, $Wert, $Name, $Icon , $color) {
+				IPS_SetVariableProfileAssociation($ProfileName, $Wert, $Name, $Icon , $color);
+	}
+
+
+    }
+?>
---
+++ AudiomaxS406System/module.php
@@ -0,0 +1,193 @@
+<?
+	class AudioMaxServerS406 extends IPSModule {
+
+        public function Create() {
+            // Diese Zeile nicht löschen.
+            parent::Create();
+
+			$this->RegisterPropertyInteger("ConnectionType", 10);
+			$this->RegisterPropertyBoolean("SendKeepAlive", true);
+			$this->RegisterPropertyInteger("SendKeepAliveInterval", 60);
+
+			$this->RegisterPropertyBoolean("SendAcknowledge", false);
+			$this->RegisterPropertyBoolean("DebugMode", false);
+			$this->RegisterPropertyBoolean("RequestPower", false);
+
+			$this->RegisterVariableBoolean("Power", "Main Power", "~Switch",0);
+			$this->EnableAction("Power");
+
+			$this->RegisterTimer("KeepAliveTimer", 0, 'AMS406_SendKeepAliveHeartbeat($_IPS[\'TARGET\']);');
+			$this->RegisterTimer("SysInfoRequestTimer", 86400 * 1000, 'AMS406_GetSysInfo($_IPS[\'TARGET\']);');
+        }
+
+
+       	public function ApplyChanges(){
+		//Never delete this line!
+		parent::ApplyChanges();
+
+		//Set Parent
+		switch($this->ReadPropertyInteger("ConnectionType")) {
+			case 10:
+				$this->ForceParent("{6DC3D946-0D31-450F-A8C6-C42DB8D7D4F1}"); //SerialPort
+				break;
+
+			case 20:
+				$this->ForceParent("{3CFF0FD9-E306-41DB-9B5A-9D06D38576C3}"); //ClientSocket
+				break;
+
+			default:
+				throw new Exception("Invalid ConnectionType for Parent");
+				break;
+		}
+
+		if($this->ReadPropertyBoolean("SendKeepAlive")) {
+			$this->SetTimerInterval("KeepAliveTimer",$this->ReadPropertyInteger("SendKeepAliveInterval")*1000);
+		}
+		else {
+			$this->SetTimerInterval("KeepAliveTimer",0);
+		}
+
+		if($this->HasActiveParent()) {
+
+			if($this->ReadPropertyBoolean("SendAcknowledge")) {
+				$this->Send("EVT;SRV;MOD;0;0");
+			}
+			else {
+				$this->Send("EVT;SRV;MOD;0;1");
+			}
+
+			if($this->ReadPropertyBoolean("DebugMode")) {
+				$this->Send("SET,SYS,DEBUG,1");
+			}
+			else {
+				$this->Send("SET,SYS,DEBUG,0");
+			}
+
+			if($this->ReadPropertyBoolean("RequestPower")) {
+				$this->Send("EVT;SRV;MOD;2;0");
+			}
+			else {
+				$this->Send("EVT;SRV;MOD;2;1");
+			}
+		}
+
+	}
+
+
+	public function RequestAction($Ident, $Value) {
+		switch($Ident){
+			case "Power":
+				switch($Value) {
+					case TRUE:
+						$this->Send("SET;SYS;PWR;1");
+						break;
+
+					case FALSE:
+						$this->Send("SET;SYS;PWR;0");
+						break;
+				}
+				SetValue($this->GetIDForIdent("Power"), $Value);
+				break;
+		}
+	}
+
+	public function Send($Text)
+	{
+			$Text = $Text.chr(13);
+			$this->SendDataToParent(json_encode(Array("DataID" => "{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}", "Buffer" => $Text)));
+	}
+
+	public function ForwardData($JSONString)
+	{
+		$data = json_decode($JSONString);
+		$this->SendDataToParent(json_encode(Array("DataID" => "{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}", "Buffer" => $data->Buffer)));
+	}
+
+
+	public function ReceiveData($JSONString)
+	{
+		$data = json_decode($JSONString);
+
+		//Kontrollieren ob Buffer leer ist.
+		$bufferData = $this->GetBuffer("DataBuffer");
+		$bufferData .= $data->Buffer;
+		$bufferParts = explode("\r\n", $bufferData);
+		//Letzten Eintrag nicht auswerten, da dieser nicht vollständig ist.
+		if(sizeof($bufferParts) > 1) {
+			for($i=0; $i<sizeof($bufferParts)-1; $i++) {
+				$this->AnalyseData($bufferParts[$i]);
+			}
+		}
+		$bufferData = $bufferParts[sizeof($bufferParts)-1];
+		//Übriggebliebene Daten auf den Buffer schreiben
+		$this->SetBuffer("DataBuffer", $bufferData);
+
+	}
+
+
+	private function AnalyseData($DataString) {
+
+		$dataArray = explode(",", $DataString);
+		$head = $dataArray[0];
+
+		switch ($head) {
+			case "EVT":
+				$command = $dataArray[1];
+
+				switch($command) {
+					case "KAL":
+						$this->RegisterVariableInteger("KAL","KAL","",1);
+						SetValue($this->GetIDForIdent("KAL"),$dataArray[2]);
+						break;
+					case "HAR":
+						$this->RegisterVariableString("HAR","HAR","",1);
+						SetValue($this->GetIDForIdent("HAR"),$dataArray[2]);
+						break;
+					case "VER":
+						$this->RegisterVariableString("VER","VER","",1);
+						SetValue($this->GetIDForIdent("VER"),$dataArray[2]);
+						break;
+					default:
+						break;
+				}
+				break;
+
+			case "SYS":
+				break;
+
+			case "AUDIO":
+						$Zone = $dataArray[3];
+						$AudioType = $dataArray[4];
+						$AudioValue = $dataArray[5];
+						$this->SendDataToChildren(json_encode(Array("DataID" => "{14802A3D-6B6D-F54E-47D6-EFC5700D7558}", "Zone" => $Zone, "AudioType" => $AudioType, "AudioValue" => $AudioValue)));
+
+						break;
+
+			default:
+				break;
+		}
+	}
+
+	public function SendKeepAliveHeartbeat() {
+		$this->Send("SET,KAL,1");
+	}
+
+	public function GetSysInfo() {
+		// to Do
+	}
+
+	public function GetConfigurationForParent() {
+		//Vordefiniertes Setup der seriellen Schnittstelle
+		if ($this->ReadPropertyInteger("ConnectionType") == 10) {
+			return "{\"BaudRate\": \"19200\", \"StopBits\": \"1\", \"DataBits\": \"8\", \"Parity\": \"None\"}";
+		}
+		else if ($this->ReadPropertyInteger("ConnectionType") == 20) {
+			return "";
+		}
+		else {
+			return "";
+		}
+	}
+
+
+?>
